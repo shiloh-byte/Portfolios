dc6eb10982cf6a6dfa8366e4482969f1c414ae7b
shiloh-byte <57815430+shiloh-byte@users.noreply.github.com>
2022-02-21 13:58:22 -0600

    Update script.js
    hanged URL to see if it will show up.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "/info.json"
+const url = "https://github.com/shiloh-byte/Portfolios/blob/main/info.json"
 fetch(url)
         .then(function (response) {
         return response.json();    
