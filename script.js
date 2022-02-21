fc7f4a25190d314209f3a4f0bbfcb50d4a9fae64
shiloh-byte <57815430+shiloh-byte@users.noreply.github.com>
2022-02-21 15:02:42 -0600

    Update script.js
    Added some formatting and changed sidebarLeft/sidebarRight to portfolioContainer.

--- script.js
+++ script.js
@@ -6,24 +6,27 @@ fetch('./static/info.json/allow-cors', {mode:'cors'})
         .then(function(data){
             console.log(data);
 
-            const output1 = document.querySelector('#sidebarLeft');
+            document.querySelector('#portfolioContainer');
             
-            output1.innerHTML = '<h2>' + data.fangGang[0].profile1[0].basics['name'] + " | " + 
+            portfolioContainer.innerHTML = '<h2>' + data.fangGang[0].profile1[0].basics['name'] + " | " + 
             data.fangGang[0].profile1[0].basics['title'] + '</h2>';
+        
 
             const myH1 = document.createElement('h5');
             myH1.innerHTML = data.fangGang[0].profile1[0].basics.social1[0].network + " : " + '<a href>' + 
             data.fangGang[0].profile1[0].basics.social1[0].url + '</a>' + " | " + data.fangGang[0].profile1[0].basics.social2[0].
             network + " : " + '<a href>' + data.fangGang[0].profile1[0].basics.social2[0].url + '</a>';
 
-            sidebarLeft.appendChild(myH1);
+            portfolioContainer.appendChild(myH1);
+        
 
             const myH2 = document.createElement('p');
             myH2.innerHTML = data.fangGang[0].profile1[0].basics['summary'];
-            sidebarLeft.appendChild(myH2);
+        
+            portfolioContainer.appendChild(myH2);
   
       
-            const output2 = document.querySelector('#sidebarRight');
+            document.querySelector('#portfolioContainer');
 
             const myDiv1 = document.createElement('div');
             const myDiv2 = document.createElement('div');
@@ -35,15 +38,15 @@ fetch('./static/info.json/allow-cors', {mode:'cors'})
   
             myDiv1.appendChild(myH3);
 
+            portfolioContainer.appendChild(myDiv1);
 
-            sidebarRight.appendChild(myDiv1);
-
+        
             const myH4 = document.createElement('h4');
             myH4.innerHTML = data.fangGang[0].profile1[0].projects2[0].name + '<br />' + '<h6>' + data.fangGang[0].
             profile1[0].projects2[0].roles + '<br /> ' + '<a href>' + data.fangGang[0].profile1[0].projects2[0].url + '</a>' + '</h6>'; 
+        
             myDiv2.appendChild(myH4);
 
-
-            sidebarRight.appendChild(myDiv2);
+            portfolioContainer.appendChild(myDiv2);
 
         }) 
